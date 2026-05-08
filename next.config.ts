ca6d7abc75d7fecb2696f5bb342f1aba918b1052
ToastNightmare <toastnightmare@gmail.com>
2026-05-09 00:35:02 +0800

    Remove experimental build worker toggle

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    webpackBuildWorker: true,
-  },
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     return config;
